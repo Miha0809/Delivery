01acb47b59d83c78b051760ef412827d2190269a
Miha0809 <miha080104@gmail.com>
2024-03-01 20:54:27 +0100

    Update dtos
    Took 2 minutes

--- Delivery/Models/DTOs/UserPrivateInfoDto.cs
+++ Delivery/Models/DTOs/UserPrivateInfoDto.cs
@@ -2,5 +2,10 @@ namespace Delivery.Models.DTOs;
 
 public class UserPrivateInfoDto
 {
-    
+    public required string Email { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required int Age { get; set; }
+    public List<Favorite>? Favorites { get; set; }
+    public List<Cart> Carts { get; set; }
 }
